92fd8571845548b17a6403553ed875e7c8602b34
shleyy10 <shleydaniel@gmail.com>
2025-07-18 15:45:23 +0100

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,3 +4,5 @@ window.supabaseConfig = {
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVubmtnYW9vaWd3a3lhZnFnY2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2NzYzNTgsImV4cCI6MjA2ODI1MjM1OH0.b7ogmi0adnadM34iHa1KdjZFMGB0vV5bw6VHcWdgh-o',
     storageBucket: 'applications'
 };
+
+console.log('Supabase config loaded:', window.supabaseConfig);
